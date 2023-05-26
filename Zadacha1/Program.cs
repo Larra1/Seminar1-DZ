315aa3e0f1f8d9244ae92bd8bd5676e2359c6e18
Ivan Odobesko <odobesko.ivan@gmail.com>
2023-05-26 15:02:02 +0300

    Решение первой задачи

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1
+// Напишите программу, которая на вход принимает два числа и выдает, какое число бальшее а какое меньшее.
+
+Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+int max = A;
+
+if (A==B)
+{
+    Console.WriteLine("Числа равны");
+}
+
+
+if (A>B)
+{
+    max=A;
+    Console.Write("Наибольшее число ");
+    Console.WriteLine(A);
+}
+
+if (A<B)
+{
+    max=B;
+    Console.Write("Наибольшее число ");
+    Console.WriteLine(B);
+}
+
+
+
+
