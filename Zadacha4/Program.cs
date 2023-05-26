78ec1be9b33f17c80e71bcbcd44658c1f7255e3e
Ivan Odobesko <odobesko.ivan@gmail.com>
2023-05-26 18:05:39 +0300

    Решение четвертой задачи

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4
+
+// Напишите программу, которая на вход принимает число N а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+int N = Math.Abs(A);
+
+Console.Write("Четные числа от 1 до ");
+Console.WriteLine(N);
+
+for (int i = 1; i<=N; i++)
+{
+    if (i%2==0)
+    {    
+        Console.Write(i+";  ");
+    }
+
+}
+
